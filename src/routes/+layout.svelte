272c5285cfc65422a387de447e74d7435168bba4
raccoon254 <tomsteve187@gmail.com>
2024-09-26 14:50:43 +0300

    Add images and --configured footer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import "../app.css";
+</script>
+
+<div class="app">
+	<main class="flex align-middle justify-center items-center flex-col">
+		<slot />
+	</main>
+</div>
+
+<style>
+</style>
