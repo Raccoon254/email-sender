2f135514ba7eb3d48ff1ae6aed9f805cb9f8f44b
raccoon254 <tomsteve187@gmail.com>
2024-09-26 15:05:37 +0300

    Complete fix the dev --url whatsapp link

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -24,11 +24,12 @@ export const actions = {
       <!-- Main Content -->
       <div style="padding: 20px;">
         <h1 style="text-align: center; color: #322c06;">Help Us Test Our Android App!</h1>
+
         <p style="font-size: 16px; line-height: 1.6;">Hello, we invite you to join our testing group to try out our new <strong>Status Saver</strong> Android app.</p>
         <p style="font-size: 16px; line-height: 1.6;">Get the app on Google Play Store or join our testing program here:</p>
         
         <!-- Call to Action Buttons -->
-        <div style="display: flex; justify-content: space-around; align-items: center; margin: 20px 0; height: 80px;">
+        <div style="display: flex; justify-content: space-around; align-items: center; margin: 20px 0; height: 40px; margin-bottom: 10px;">
           <!-- Google Play Store Button -->
           <a href="https://play.google.com/store/apps/details?id=com.kentom.saver" style="display: inline-block; text-decoration: none;">
             <img src="https://status-saver.vercel.app/store.png" alt="Get it on Google Play" style="width: 180px;" />
@@ -36,11 +37,11 @@ export const actions = {
         </div>
         
         <!-- Compensation Note -->
-        <p style="font-size: 16px; line-height: 1.6;">As a token of appreciation, you will be <strong>financially compensated</strong> for your feedback. To claim your payment, please reach out to the app developer using the link below.</p>
+        <p style="font-size: 16px; margin-top: 20px; line-height: 1.6;">As a token of appreciation, you will be <strong>financially compensated</strong> for your feedback. To claim your payment, please reach out to the app developer using the link below.</p>
         
         <!-- WhatsApp Contact Button -->
         <div style="text-align: center; margin: 20px 0;">
-          <a href="https://wa.me/15551234567" style="display: inline-block; background-color: ; color: black; padding: 10px 25px; border-radius: 100px; text-decoration: none; font-size: 18px;
+          <a href="https://wa.me/254758481320" style="display: inline-block; background-color: ; color: black; padding: 10px 25px; border-radius: 100px; text-decoration: none; font-size: 18px;
           border: 2px solid #25d366; transition: background-color 0.3s; cursor: pointer; text-align: center; margin: 10px 0; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
           ">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/512px-WhatsApp.svg.png" alt="WhatsApp Logo" style="width: 30px; vertical-align: middle; margin-right: 10px;" /> Contact Developer
